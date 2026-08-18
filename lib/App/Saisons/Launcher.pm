bc691438a9628587efa82190392a718a9b457088
Nicolas Mendoza <mendoza@pvv.ntnu.no>
2026-08-18 12:26:35 +0200

    Clean up Launcher: collapse AppleScript into one-liners, remove comments

--- lib/App/Saisons/Launcher.pm
+++ lib/App/Saisons/Launcher.pm
@@ -1,13 +1,9 @@
 package App::Saisons::Launcher;
 use strict;
 use warnings;
-
-# Launch a shell command in a new terminal tab/window according to $launcher.
-# $title is used as the tab/window title where supported.
 sub launch_cmd {
     my ($cmd, $cwd, $launcher, $title) = @_;
     $title //= 'saisons';
-
     if ($launcher eq 'tmux') {
         system('tmux', 'new-window', '-c', $cwd, $cmd);
     }
@@ -21,38 +17,20 @@ sub launch_cmd {
     }
     elsif ($launcher eq 'iterm') {
         my $escaped = _applescript_quote($cmd);
-        my $script = qq{
-            tell application "iTerm2"
-                tell current window
-                    create tab with default profile
-                    tell current session of current tab
-                        write text $escaped
-                    end tell
-                end tell
-            end tell
-        };
-        system('osascript', '-e', $script);
+        system('osascript', '-e', qq{tell application "iTerm2"\ntell current window\ncreate tab with default profile\ntell current session of current tab\nwrite text $escaped\nend tell\nend tell\nend tell});
     }
     elsif ($launcher eq 'terminal-app') {
         my $escaped = _applescript_quote($cmd);
-        my $script = qq{
-            tell application "Terminal"
-                do script $escaped
-                activate
-            end tell
-        };
-        system('osascript', '-e', $script);
+        system('osascript', '-e', qq{tell application "Terminal"\ndo script $escaped\nactivate\nend tell});
     }
     else {
         system('bash', '-c', $cmd);
     }
 }
-
 sub _applescript_quote {
     my ($str) = @_;
     $str =~ s/\\/\\\\/g;
     $str =~ s/"/\\"/g;
     return qq{"$str"};
 }
-
 1;
